233275a8563b33d5d86efafa2d3ae6df0b43c552
Dani Lyn <dlyn1110@gmail.com>
2018-05-14 17:48:38 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,7 +9,7 @@ attr_accessor :project, :name, :backed_projects
   end
 
  def back_project(project)
-   @backed_projects << project
+   backed_projects << project
    project.backers << self
  end
 
